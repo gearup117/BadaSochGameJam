0b703b9fe7cd1a3d507962fd962c58bf8f71143d
gearup117 <yashwankhade987@gmail.com>
2020-08-29 18:56:34 +0530

    idk

--- BadaSoch/Assets/Scripts/CameraMove.cs
+++ BadaSoch/Assets/Scripts/CameraMove.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public Animator anim;
     public Transform player;
     public float height;
     public float smooth;
      Vector3 velocity = Vector3.zero;
+    private Gun gun;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gun = player.GetComponentInChildren<Gun>();
     }
 
     // Update is called once per frame
@@ -22,5 +24,12 @@ public class CameraMove : MonoBehaviour
         pos.y = player.position.y + height;
         pos.z = player.position.z - 10f;
         transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+        if (gun.anim.GetBool("shoot"))
+        {
+            anim.SetBool("Shake", true);
+        }
+        else {
+            anim.SetBool("Shake", false);
+        }
     }
 }
